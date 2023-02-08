deda254a9edbce8ab98b93c79a951601d41bf3d9
box-o-water <boxooowater@gmail.com>
2023-02-08 16:35:38 -0600

    feat(dashboard-and-auth): can login and logout, create and delete posts

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ const htmlRoutes = require("./htmlRoutes");
 router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>");
-});
-
 module.exports = router;
